d69affc6175a200166c5b0ab1d8b0b623a086145
drole <genuines.forever@gmail.com>
2024-01-21 17:53:52 +0400

    feat: add solution to files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const {stdout} = process;
+
+const dirPath = path.join(__dirname, 'secret-folder');
+
+
+fs.readdir(dirPath,
+  {encoding: 'utf8', withFileTypes: true},
+  (err, files) => {
+    if (err) {
+      stdout.write(err)
+    } else {
+      files.forEach(element => {
+        //console.log(element);
+        //console.log(element.isFile());
+        if (element.isFile()) {
+          let ext = path.extname(element.name);
+          let name = path.basename(element.name, ext);
+          fs.stat(path.join(dirPath, element.name), (err, stats) => {
+            stdout.write(`${name} - ${ext.slice(1)} - ${stats.size}b\n`);
+          })
+        }
+      });
+    }
+});
