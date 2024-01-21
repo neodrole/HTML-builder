983468fcd6a05edaf258cdcacabe6472a1b28d83
drole <genuines.forever@gmail.com>
2024-01-21 13:24:21 +0400

    feat: add solution to read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+const {stdout} = process;
+
+const filePath = path.join(__dirname, "text.txt");
+const readableStream = fs.createReadStream(filePath, "utf-8");
+
+readableStream.on("data", (chunk) => {
+  stdout.write(chunk);
+});
